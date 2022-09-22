bb02cb398e6b7a24d11665445aca9dadf182ec74
huerqiang <13766876214@163.com>
2022-09-23 00:14:24 +0800

    fix gene length bug in `countToTpm_matrix`

--- R/fpkm_count_conversion.r
+++ R/fpkm_count_conversion.r
@@ -62,6 +62,7 @@ countToFpkm_matrix <- function(counts_matrix) {
 #'
 #' @param counts_matrix a matrix, colnames of counts_matrix are sample name,
 #' rownames of counts_matrix are gene symbols
+#' @param keyType keyType
 #'
 #' @return a matrix
 #' @export
@@ -71,11 +72,19 @@ countToFpkm_matrix <- function(counts_matrix) {
 #' rownames(lung_squ_count2) <- c("DISC1","TCOF1","SPPL3")
 #' colnames(lung_squ_count2) <- c("sample1","sample2","sample3")
 #' jieguo <- countToTpm_matrix(lung_squ_count2)
-countToTpm_matrix <- function(counts_matrix) {
-    genes_count <- intersect(rownames(counts_matrix),gene_loc_len[,1])
+countToTpm_matrix <- function(counts_matrix, keyType = "SYMBOL") {
+    gene_cov2 <- gene_cov
+    if (keyType != "ENTREZID") {
+        genes_bitr <- clusterProfiler::bitr(rownames(gene_cov), fromType = "ENTREZID", toType = keyType, 
+            OrgDb = org.Hs.eg.db::org.Hs.eg.db, drop = TRUE)
+        genes_bitr <- genes_bitr[!duplicated(genes_bitr[,2]), ]
+        gene_cov2 <- gene_cov[genes_bitr$ENTREZID, ]
+        rownames(gene_cov2) <- genes_bitr[, 2]
+    }
+    genes_count <- intersect(rownames(counts_matrix), rownames(gene_cov2))
     counts_matrix_new <- counts_matrix[genes_count,]
-    gene_loc_len_new <- gene_loc_len[genes_count,]
-    genes_length <- as.numeric(gene_loc_len_new[,4])
+    gene_loc_len_new <-gene_cov2[genes_count,]
+    genes_length <- as.numeric(gene_loc_len_new[,1])
     counts_matrix_new2 <- counts_matrix_new
     for(i in seq_len(dim(counts_matrix_new2)[2])) {
         counts_matrix_new2[,i] <- countToTpm(as.numeric(counts_matrix_new2[,i]),genes_length)
